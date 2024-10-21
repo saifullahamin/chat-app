305bd33da3b0d8a11c1937796ee6e6064a0a1002
Saifullah Amin <saifullahaminkhan@gmail.com>
2024-10-21 00:10:28 -0700

    Changed the encryption logic

--- src/utils/encryption.ts
+++ src/utils/encryption.ts
@@ -3,30 +3,27 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY || crypto.randomBytes(32);
+const ENCRYPTION_KEY = Buffer.from(process.env.ENCRYPTION_KEY!, 'hex');
 const IV_LENGTH = 16;
 
 export const encrypt = (text: string): string => {
   const iv = crypto.randomBytes(IV_LENGTH);
-  const cipher = crypto.createCipheriv('aes-256-gcm', ENCRYPTION_KEY, iv);
+  const cipher = crypto.createCipheriv('aes-256-cbc', ENCRYPTION_KEY, iv);
 
   let encrypted = cipher.update(text, 'utf8', 'hex');
   encrypted += cipher.final('hex');
-  const tag = cipher.getAuthTag().toString('hex');
 
-  return `${iv.toString('hex')}:${encrypted}:${tag}`;
+  return `${iv.toString('hex')}:${encrypted}`;
 };
 
 export const decrypt = (encrypted: string): string => {
-  const [iv, encryptedText, tag] = encrypted.split(':');
+  const [iv, encryptedText] = encrypted.split(':');
 
   const decipher = crypto.createDecipheriv(
-    'aes-256-gcm',
+    'aes-256-cbc',
     ENCRYPTION_KEY,
     Buffer.from(iv, 'hex')
   );
-
-  decipher.setAuthTag(Buffer.from(tag, 'hex'));
 
   let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
   decrypted += decipher.final('utf8');
